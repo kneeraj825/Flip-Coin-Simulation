a270458f1cf50d9e86b9953b37f0e470ef2d9b4e
kneeraj825 <kneeraj825@gmail.com>
2020-02-17 14:31:24 +0530

    Added:Checking the coin flip is heads or tails

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
 
 echo Welcome
+#checking the coin flip is either heads or tails
+ISHEAD=1
+coinCheck=$(( RANDOM%2 ))
+if (( $ISHEAD == $coinCheck ))
+then
+	echo "Heads"
+else
+	echo "Tails"
+fi
