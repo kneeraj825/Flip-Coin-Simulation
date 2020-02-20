46f5255ae54f9c868b75471edd1e3adbcf82de8e
kneeraj825 <kneeraj825@gmail.com>
2020-02-20 11:33:25 +0530

    Added:Sorting the combination and finding the winner combination

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,47 +2,60 @@
 
 #constant
 ISFLIP=0
+SINGLET=1
+DOUBLET=2
 TRIPLET=3
 
-# dictionary to store triplet combination of flip coin
-declare -A tripletFlip
+#dictionar to store doublet combination of flip coin
+declare -A doubletFlip
 #taking user input for number of coin flips
-read -p "Enter the Number of Coin Flip : " numberOfCoinFlip
-
-#storing the combine value in dictionary
+read -p "Enter the Number of Coin Flip : " numberOfFlip
+#storing combine value in dictionary
 function coinFlip()
 {
-   local NUMBER_OF_COIN=$1
-   for(( flip=0; flip<$numberOfCoinFlip; flip++ ))
-   do
-      for(( coin=0; coin<$NUMBER_OF_COIN; coin++ ))
-      do
-         randomFlip=$(( RANDOM % 2 ))
-         if (($randomFlip == $ISFLIP ))
-         then
-            coinSide+=H
-         else
-            coinSide+=T
-         fi
-      done
-            ((tripletFlip[$coinSide]++))
-            coinSide=""
-   done
+	numberOfCoinFlip=$1
+	local NUMBER_OF_COIN=$2
+	for(( flip=0; flip<$numberOfCoinFlip; flip++ ))
+	do
+		for(( coin=0; coin<$NUMBER_OF_COIN; coin++ ))
+		do
+			randomFlip=$(( RANDOM % 2 ))
+			if (($randomFlip == $ISFLIP ))
+			then
+				coinSide+=H
+			else
+				coinSide+=T
+			fi
+		done
+		((doubletFlip[$coinSide]++))
+		coinSide=""
+    done
+}
+function sorting() {
+	for k in ${!doubletFlip[@]}
+	do
+		echo $k '-' ${doubletFlip[$k]}
+	done |
+	sort -rn -k3
 }
 
 #calculating percentage 
 function calculatePercentage()
 {
-  # local NUMBER_OF_COIN=$1
-   for countOfIndex in ${!tripletFlip[@]}
-   do
-      tripletFlip[$countOfIndex]=`echo "scale=2; ${tripletFlip[$countOfIndex]}/$numberOfCoinFlip*100" | bc`
-   done
-      echo ${tripletFlip[@]}
+	local NUMBER_OF_COIN=$1
+	for countOfIndex in ${!doubletFlip[@]}
+	do
+		doubletFlip[$countOfIndex]=`echo "scale=2; ${doubletFlip[$countOfIndex]}/$numberOfCoinFlip*100" | bc`
+	done
+	echo ${doubletFlip[@]}
 }
-coinFlip $TRIPLET
-echo ${!tripletFlip[@]}
-echo ${tripletFlip[@]}
-result=$(calculatePercentage $numberOfCoinFlip)
-
+coinFlip $numberOfFlip $SINGLET
+coinFlip $numberOfFlip $DOUBLET
+coinFlip $numberOfFlip $TRIPLET
+echo ${!doubletFlip[@]}
+echo ${doubletFlip[@]}
+result1=$(calculatePercentage $numberOfCoinFlip)
+echo "percentage:$result1"
+echo "$(sorting)"
+echo "Winner is:-  $(sorting | head -1)"
 
